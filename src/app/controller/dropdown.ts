a91b2ba8c060d84522a8877d1741b2d30e0edeae
QuyenLS <danquyen6596@gmail.com>
2019-07-30 17:20:25 +0700

    Add new update function getParentNode

--- src/app/controller/dropdown.ts
+++ src/app/controller/dropdown.ts
@@ -15,24 +15,31 @@ export class DropDown extends wjInput.Menu {
     }
 
     set itemsSource(pItems: any) {
+        console.log(pItems);
         if (this._itemsSource && this._itemsSource.length > 0) {
             this._itemsSource.forEach((_item, _i) => {
                 if (_item.propertyChanged)
                     _item.propertyChanged.removeAllHandlers();
             });
+            console.log("1");
         }
 
         if (pItems instanceof wjCore.ObservableArray) {
             this._itemsSource = pItems;
+            console.log("2");
         }
         else {
             this._itemsSource = new wjCore.ObservableArray(...pItems);
+            console.log(this._itemsSource);
+            console.log("3");
         }
 
         if (this._lbx.itemsSource != this._itemsSource) {
             this._lbx.itemsSource = this._itemsSource;
             this.onItemsSourceChanged();
+            console.log("4");
         }
+        console.log(this._itemsSource);
         this._updateBtn();
     }
 
--- src/app/wijmo-grouping/wijmo-grouping.component.ts
+++ src/app/wijmo-grouping/wijmo-grouping.component.ts
@@ -10,6 +10,7 @@ import { UserService } from '../user.service';
 import { WjComponentResolvedMetadata } from 'wijmo/wijmo.angular2.directiveBase';
 import { getDate } from 'ngx-bootstrap/chronos/utils/date-getters';
 import { createVerify } from 'crypto';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-wijmo-grouping',
@@ -26,32 +27,39 @@ export class WijmoGroupingComponent implements OnInit {
   _bIsChecked = false;
   _totalColumns = [];
   _columnAvailable = ['ItemCode', 'ItemName', 'Unit', 'OpenInventory', 'OpenAmount'];
-  _groupDescription = ['ItemName', 'Unit'];
+  _groupDescription = ['ItemName'];
   cv = new wjCore.CollectionView;
   gd = new wjCore.PropertyGroupDescription('ItemName')
-  
-  
+  hostElem: HTMLElement;
+
+
 
   constructor(private userService: UserService,
     private elRef: ElementRef) {
-    
+
   }
 
   ngOnInit() {
     this.getInitData();
+    this.hostElem = document.getElementById('menuControl');
+    let drop = new DropDown(this.hostElem, this._groupMenuItems);
+    console.log(this.hostElem);
+    console.log(drop)
 
   }
 
   getInitData() {
- 
+    // this._groupDescription.forEach((item) => {
+    //   this.cv.groupDescriptions.push(new wjCore.PropertyGroupDescription(item));
+    // })
     this.cv.groupDescriptions.push(this.gd);
-    this.cv.groupDescriptions.push(new wjCore.PropertyGroupDescription('Unit'));
     this.userService.getData().subscribe(data => {
       this._dataSources = data.splice(0, 1000);
-      this.initializeMenu(this._dataSources);
-      this.groupData.groups.forEach((item, idx) => {
+      this.cv.sourceCollection = this._dataSources;
+
+      this.cv.groups.forEach((item) => {
         this._groupMenuItems.push(item);
-      });
+      })
 
       this.groupMenu.itemsSource = this._groupMenuItems;
 
@@ -59,25 +67,18 @@ export class WijmoGroupingComponent implements OnInit {
       for (const key in _item) {
         if (_item.hasOwnProperty(key)) {
           this._totalColumns.push(key);
-
         }
       }
     })
   }
 
 
-  initializeMenu(data: any) {
-    this.groupData = new wjCore.CollectionView(data, {
-      groupDescriptions: this._groupDescription
-    });
-
-    this.groupMenu.maxDropDownHeight = 300;
+  initializeMenu(menu: wjInput.Menu) {
+    this.groupMenu.maxDropDownHeight = 250;
   }
 
   @ViewChild('groupItems', { static: false }) groupItem: wjGrid.FlexGrid;
   @ViewChild('groupMenu', { static: false }) groupMenu: wjInput.Menu;
-
-
 
   columnAvailableChange(event: any) {
     if (event.target.checked) {
@@ -96,53 +97,93 @@ export class WijmoGroupingComponent implements OnInit {
 
   groupDescriptionChanged(event: any) {
     if (event.target.checked) {
-      this._groupDescription.push(event.target.value);
-      this.initializeMenu(this._dataSources);
+      this.cv.groupDescriptions.push(new wjCore.PropertyGroupDescription(event.target.value));
       return;
     }
-
-    this._groupDescription.splice(this._groupDescription.indexOf(event.target.value), 1);
-    this.initializeMenu(this._dataSources);
+    this.cv.groupDescriptions.forEach((item, idx) => {
+      if (item.propertyName === event.target.value) {
+        this.cv.groupDescriptions.splice(idx, 1);
+      }
+    })
   }
 
-  checkgroupDes(col: string) {
-    if (this._groupDescription.indexOf(col) === -1)
-      return false;
-    return true;
+  checkgroupDes(col: string): boolean {
+    for (let i = 0; i < this.cv.groupDescriptions.length; i++) {
+      if (this.cv.groupDescriptions[i].propertyName === col) {
+        return true;
+      }
+    }
+    return false;
   }
 
   onClickItem(menu: wjInput.Menu, grid: wjGrid.FlexGrid) {
     let _menuItem = menu.selectedItem;
-    
-    for (let i = menu.selectedIndex*this._groupDescription.length; i < grid.rows.length; i++) {
+
+    for (let i = menu.selectedIndex * this.cv.groupDescriptions.length; i < grid.rows.length; i++) {
       let item = grid.rows[i];
       if (item instanceof wjGrid.GroupRow) {
-        
-        if(item.dataItem.name === _menuItem.name) {
+
+        if (item.dataItem.name === _menuItem.name) {
           grid.select(new wjGrid.CellRange(i, 0), true);
           return;
         }
       }
     }
-    grid.select(new wjGrid.CellRange(28, 0), true);
-    
   }
 
   flexInitialized(flex: wjGrid.FlexGrid, menu: wjInput.Menu) {
+    flex.itemsSource = this.cv;
+
     flex.selectionChanged.addHandler((s, e: wjGrid.FormatItemEventArgs) => {
-      let item = flex.selectedRows[0];
-      if (s.rows[e.row] instanceof wjGrid.GroupRow) {
-        console.log(item.dataItem.isBottomLevel);
-      }
-      console.log(item);
-      console.log(menu);
-      console.log(this.cv.groupDescriptions)
-
-
+        let row = flex.selectedRows[0];
+        let currentNode = [];
+        this.getAllParentNode(row, currentNode);
+        menu.text = currentNode[currentNode.length - 1].dataItem.name;
     })
+
+    new DropDown(this.hostElem, this.cv.sourceCollection);
   }
 
-  
+  public getAllParentNode(row, currentNode: any) {
+    let parentNode = this.getParentNode(row);
+    if (parentNode != null && parentNode.level > 0) {
+      currentNode.push(parentNode)
+      this.getAllParentNode(parentNode, currentNode);
+    }
 
-  
+    if (isNull(parentNode)) {
+      currentNode.push(row)
+      return;
+    }
+
+    if (parentNode.level === 0) {
+      currentNode.push(parentNode);
+      return;
+    }
+
+  }
+
+  public getAllNodeIsLevel(row) {
+
+  }
+
+  public getParentNode(row) {
+    // get row level
+    let startLevel = row instanceof (wjGrid.GroupRow) ? row.level : null;
+    let startIndex = row.index;
+
+    // travel up to find parent node
+    for (let i = startIndex - 1; i >= 0; i--) {
+      let thisRow = row.grid.rows[i],
+        thisLevel = thisRow instanceof (wjGrid.GroupRow) ? thisRow.level : null;
+
+      if (thisLevel != null) {
+        if (startLevel == null || (startLevel > -1 && thisLevel < startLevel))
+          return thisRow;
+      }
+    }
+
+    // not found
+    return null;
+  };
 }
